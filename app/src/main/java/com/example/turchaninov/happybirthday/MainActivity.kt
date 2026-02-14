26e0b02fedad38ca19e2c356a2e74391d8a84506
TurchaninovStudent <aeturchaninov@ya.ru>
2026-02-14 12:24:50 +0300

    Add Box layout

--- app/src/main/java/com/example/turchaninov/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/turchaninov/happybirthday/MainActivity.kt
@@ -6,11 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,11 +29,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GreetingText(
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(
                         message = "Happy Birthday Наиль!",
-                        from = "Андрей",
-                        modifier = Modifier.padding(innerPadding)
+                        from = "от Андрея"
                     )
                 }
             }
@@ -66,17 +69,27 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.heatmapimage)
-
-    Image(
-        painter = image,
-        contentDescription = null
-    )
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage("Happy Birthday Наиль!", "от Андрея")
+        GreetingImage(
+            message = "Happy Birthday Наиль!",
+            from = "от Андрея"
+        )
     }
 }
